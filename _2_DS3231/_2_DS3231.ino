c3adff6da15bcfd92ec26ab55c05755c1d39e358
qzane <qzane@live.com>
2015-03-29 00:05:12 +0800

    write BMP180.h

--- _2_DS3231/_2_DS3231.ino
+++ _2_DS3231/_2_DS3231.ino
@@ -1,8 +1,8 @@
 #include <Wire.h>
 #include "DS3231.h"
-
+#include "BMP180.h"
 DS3231 RTC; //Create the DS3231 object
-
+BMP180 bmp;
 char weekDay[][4] = {"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat" };
 
 //year, month, date, hour, min, sec and week-day(starts from 0 and goes to 6)
@@ -16,6 +16,7 @@ void setup ()
     Serial.begin(9600);
     Wire.begin();
     RTC.begin();
+    bmp.begin();
     serialTime = 0;
     RTC.convertTemperature();             //convert current temperature into registers
     Serial.println("Ready!");
@@ -41,6 +42,10 @@ void setTime(){
       RTC.convertTemperature();             //convert current temperature into registers
       Serial.print(RTC.getTemperature()); //read registers and display the temperature
       Serial.println("deg C");
+      float t,p;
+      bmp.read(t,p);
+      Serial.println(t);
+      Serial.println(p);
 }
 
 void loop () 
