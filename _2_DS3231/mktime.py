11b991af7f869861feebebe202c8a4be8253bb93
qzane <qzane@live.com>
2015-03-31 15:58:31 +0800

    mktime add checkTime

--- _2_DS3231/mktime.py
+++ _2_DS3231/mktime.py
@@ -2,7 +2,7 @@
 import time
 import serial
 global ser
-
+ser = None
 def init():
     global ser
     for i in xrange(0,20):
@@ -18,15 +18,45 @@ def init():
             
 def setTime(Time=None):
     global ser
+    if not ser:
+        init()
     if not Time:
         ser.open()
         time.sleep(3)
+        ser.flushInput()
+        ser.write('101\n')
+        ser.flush()
+        print 'connecting...'
+        while(True):
+            if(ser.inWaiting()!=0 and ser.read(1)=='2'):
+                break
         Time = time.time()
-        ser.write('%d %d\n'%(101,Time));
+        ser.write('%d\n'%(Time+1))
+        ser.flush()
         time.sleep(0.3)
         print ser.read(ser.inWaiting())
         ser.close()
         
+def checkTime():
+    global ser
+    if not ser:
+        init()
+    ser.open()
+    time.sleep(3)
+    ser.flushInput()
+    ti1 = time.time()
+    ser.write('103\n')
+    ser.flush()
+    ti = time.time()
+    time.sleep(0.3)
+    a = ser.read(ser.inWaiting())
+    ser.close()
+    a = a.split('\n')
+    w = [int(i) for i in a[:6]]+[0,0,0]
+    tt = time.mktime(w)
+    print tt-ti,tt,ti,ti-ti1
+    return tt-ti
+    
 if __name__ == '__main__':
     init()
     setTime()
